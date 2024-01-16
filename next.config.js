6eadad4ecaef74fc686685990d8f727976c87416
ozhanm <ozhan.mengucek@carboninteraktif.com>
2024-01-16 22:00:30 +0300

    Vercel config ayarlandı

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 const path = require('path');
 
 const nextConfig = {
-    // output: 'export',
-    basePath: process.env.NEXT_PUBLIC_BASE_URL,
+    basePath: '.',
     trailingSlash: false,
     reactStrictMode: true,
     sassOptions: {
